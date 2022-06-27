af03dd3f7daa639433d73cb65aeb1304f0e8b49a
Covali Andrei <krab4ek@gmail.com>
2022-06-27 17:49:43 +0300

    Add project files.

---
+++ TimeUtilClass/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.WriteLine("***** Fun with Static Classes ****** \n");
+TimeUtilClass.PrintDate();
+TimeUtilClass.PrintTime();
+
+
+static class TimeUtilClass
+{
+    public static void PrintTime()
+        => Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+    public static void PrintDate()
+    {
+        Console.WriteLine(DateTime.Now.Date);
+    }
+}
